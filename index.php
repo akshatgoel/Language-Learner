fb523c1a8f48a7d47f6904b9bf1e9a4346d7f837
unknown <akshat@akshat-PC.(none)>
2012-07-07 14:24:00 +0530

    set word of the day for all languages

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 // Defined in 'AppInfo.php'
 
 require_once('AppInfo.php');
-require_once('lib/library.php') or die 'fdfdfdfdfd';
+require_once('lib/library.php');
 // Enforce https on production
 if (substr(AppInfo::getUrl(), 0, 8) != 'https://' && $_SERVER['REMOTE_ADDR'] != '127.0.0.1') {
   header('Location: https://'. $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
