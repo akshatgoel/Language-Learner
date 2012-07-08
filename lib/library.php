a4b89dfe94e7f6df519979e76acf9c5db2c7144c
unknown <akshat@akshat-PC.(none)>
2012-07-08 11:54:40 +0530

    mailer

--- lib/library.php
+++ lib/library.php
@@ -364,6 +364,314 @@
     return $arr;
   }//get_notify_users
   
+  function get_mailer_words($data['default_language']){
+		$word = top_words($data['default_language'],10);
+		if(empty($word))
+			continue;
+		$header = '<!-- Wrapper -->
+<table width="100%" height="100%" border="0" cellpadding="0" cellspacing="0" align="center">
+	<tr>
+		<td width="100%" height="100%" background="images/bg.jpg" valign="top">	
+
+		<!-- Main wrapper -->
+		<table width="100%" border="0" cellpadding="0" cellspacing="0" align="center">
+			<tr>
+				<td valign="top">
+				
+					<!-- Cant view this email? -->
+					<table width="602" border="0" cellpadding="0" cellspacing="0" align="center">
+						<tr>
+							<td mc:edit="cant_view" height="80" style="text-align: center; font-style: italic; font-size: 11px; color: #AAAAAA; font-weight: normal; text-align: center; font-family: Georgia, serif; text-shadow: 1px 1px 1px #FFFFFF;">
+								
+								<a href="https://apps.facebook.com/lang_learn/" style="color: #AAAAAA; text-decoration: none;">Can\'t view Email?</a>
+							
+							
+							</td>
+						</tr>
+					</table><!-- End Cant view this email? -->
+					
+							
+					<article class="stampReadySkew"><style type="text/css">.stampReadyEffect {opacity: 1;-webkit-animation-name: stampReadyEffect;-webkit-animation-duration: 2.1s;-webkit-animation-iteration-count: 1;-webkit-animation-timing-function: linear; }@-webkit-keyframes stampReadyEffect { 1% { opacity: 0; -webkit-transform: scale(0.2);}80% { opacity: 0; -webkit-transform: scale(0.2);}95% { opacity: 1; -webkit-transform: scale(1.1);}100% { opacity: 1; -webkit-transform: scale(1);}}</style></article><header class="stampReadyEffect">
+					<table width="602" class="stampReady" border="0" cellpadding="0" cellspacing="0" align="center">
+						<tbody><tr>
+							<td width="602" style="line-height: 1px;">
+								<img src="images/pop-up_top.jpg" alt="" border="0" style="display: block;">									
+							</td>
+						</tr>
+					</tbody></table>
+							
+					<!-- Pop-up Text -->
+					<table width="602" border="0" cellpadding="0" cellspacing="0" align="center">
+						<tbody><tr>
+							<td width="1" height="40" bgcolor="#28a2cc"></td>
+							<td width="600" height="40" bgcolor="#88d5ef">
+								
+								<!-- Text -->
+								<table width="600" border="0" cellpadding="0" cellspacing="0" align="center" style="font-size: 14px; color: #2081a1; font-weight: bold; text-align: left; font-family: Helvetica, Arial, sans-serif; line-height: 20px; text-shadow: 0px 1px 1px #ffefcd;">
+									<tbody><tr>
+										<td width="20"></td>
+										<td width="545" mc:edit="discount">Howdy '.$name.'!</td>
+										<td width="15">
+											<img mc:edit="cross" src="images/cross.jpg" alt="" border="0" style="display: block;">
+										</td>
+										<td width="20"></td>
+									</tr>
+								</tbody></table>
+
+							</td>
+							<td width="1" height="40" bgcolor="#28a2cc"></td>
+						</tr>
+					</tbody></table><!-- End Pop-up Text -->
+					
+					<!-- Pop-up Border Bottom Image -->
+					<table width="602" border="0" cellpadding="0" cellspacing="0" align="center">
+						<tbody><tr>
+							<td width="602" style="line-height: 1px;">
+								<img src="images/pop-up_bottom.jpg" alt="" border="0" style="display: block;">									
+							</td>
+						</tr>
+					</tbody></table><!-- End Pop-up Border Bottom Image -->
+					
+					<!-- Pop-up Shadow -->
+					<table width="602" border="0" cellpadding="0" cellspacing="0" align="center">
+						<tbody><tr>
+							<td width="602" style="line-height: 1px;">
+								<img src="images/pop-up_shadow.jpg" alt="" border="0" style="display: block;">									
+							</td>
+						</tr>
+					</tbody></table><!-- End Pop-up Shadow -->
+					</header>
+
+					<!-- Empty Table -->
+					<table width="602" border="0" cellpadding="0" cellspacing="0" align="center">
+						<tr>
+							<td width="602" height="20">									
+							</td>
+						</tr>
+					</table>
+				';
+				
+$footer = '			
+								<!-- Dashed Border -->
+								<table width="602" border="0" cellpadding="0" cellspacing="0" align="center">
+									<tr>
+										<td width="1" height="1" bgcolor="#dcdcdc"></td>
+										<td width="600" height="1" style="border-bottom: 1px dashed #d4d4d4"></td>
+										<td width="1" height="1" bgcolor="#dcdcdc"></td>
+									</tr>
+								</table>
+								
+								<!-- Empty Table -->
+								<table width="602" border="0" cellpadding="0" cellspacing="0" align="center" bgcolor="#FFFFFF">
+									<tr>
+										<td width="1" height="1" bgcolor="#dcdcdc"></td>
+										<td width="600" height="8" style="line-height: 1px;">
+											<img src="images/blank.gif" alt="" style="display: block;">									
+										</td>
+										<td width="1" height="1" bgcolor="#dcdcdc"></td>
+									</tr>
+								</table>
+								
+								
+							</td>
+						</tr>
+					</table><!-- End White Color Wrapper -->
+					
+					<!-- Empty Table -->
+					<table width="602" border="0" cellpadding="0" cellspacing="0" align="center">
+						<tr>
+							<td width="602" height="8">
+								<img src="images/blank.gif" alt="" border="0" style="display: block;">
+							</td>
+						</tr>
+					</table>
+
+		
+				</td>
+			</tr>
+		</table><!-- End Main wrapper -->
+
+		</td>
+	</tr>
+</table><!-- End Wrapper -->';
+
+		$text = '<!-- Shadow --> 
+								<table width="602" border="0" cellpadding="0" cellspacing="0" align="center">
+									<tr>
+										<td width="1" bgcolor="#dcdcdc"></td>
+										<td width="600" bgcolor="#dcdcdc">
+										
+										<!-- Middle Shadow -->
+										<table width="600" border="0" cellpadding="0" cellspacing="0" align="center">
+											<tr>
+												<td width="600" style="line-height: 1px;" valign="top">
+													<img src="images/middle_shadow.jpg" alt="" border="0" style="display: block;">								
+												</td>
+											</tr>
+										</table>
+										
+										</td>
+										<td width="1" bgcolor="#dcdcdc"></td>
+									</tr>
+								</table>
+								
+								<!-- Start 2 Columns -->
+								<table width="602" border="0" cellpadding="0" cellspacing="0" align="center" bgcolor="#FFFFFF">
+									<tr>
+										<td width="1" bgcolor="#dcdcdc"></td>
+										<td width="31"></td>
+										<td width="539">
+										
+											<!-- Empty Table -->
+											<table width="539" border="0" cellpadding="0" cellspacing="0" align="center" bgcolor="#FFFFFF">
+												<tr>	
+													<td width="539" height="20" style="line-height: 1px;">
+														<img src="images/blank.gif" alt="" style="display: block;">	
+													</td>							
+												</tr>
+											</table>
+											
+											
+									
+											
+											<!-- 2 Colums Text -->
+											<table width="539" border="0" cellpadding="0" cellspacing="0" align="center" style="font-size: 14px; color: #b8b8b8; text-align: left; font-family: Helvetica, Arial, sans-serif; line-height: 20px;">
+												<tr>
+													<td width="249" mc:edit="column_text1" valign="top" mc:edit="text1"><a href="'.$siteUrl.'" style="text-decoration: none; color: #707070; font-weight: bold;">'.$word[0]['word'].'</a><br />'.$word[0]['translation'].'</td>
+													<td width="41"></td>
+													<td width="249" mc:edit="column_text2" valign="top" mc:edit="text2"><a href="'.$siteUrl.'" style="text-decoration: none; color: #707070; font-weight: bold;">'.$word[1]['word'].'</a><br />'.$word[1]['translation'].'</td>
+												</tr>
+											</table><!-- End 2 Colums Text -->
+											
+											<!-- Empty Table -->
+											<table width="539" border="0" cellpadding="0" cellspacing="0" align="center">
+												<tr>
+													<td width="539" height="20" style="line-height: 1px;">
+														<img src="images/blank.gif" alt="" style="display: block;">									
+													</td>
+												</tr>
+											</table>
+										
+											<!-- Preview Buttons -->
+											<table width="539" border="0" cellpadding="0" cellspacing="0" align="center">
+												<tr>
+													<td width="249" height="40" mc:edit="preview_button" valign="top">
+														<a href="'.$siteUrl.'"><img mc:edit="button" src="images/preview_button.jpg" alt="" style="display: block;" border="0"></a>
+													</td>
+													<td width="41" height="40"></td>
+													<td width="249" height="40" mc:edit="preview_button" valign="top">
+														<a href="'.$siteUrl.'"><img mc:edit="button" src="images/preview_button.jpg" alt="" style="display: block;" border="0"></a>
+													</td>
+												</tr>
+											</table><!-- End Preview Button -->
+											
+											
+											<!-- Empty Table -->
+											<table width="539" border="0" cellpadding="0" cellspacing="0" align="center" bgcolor="#FFFFFF">
+												<tr>
+													<td width="539" height="30" style="line-height: 1px;">
+														<img src="images/blank.gif" alt="" style="display: block;">									
+													</td>
+												</tr>
+											</table>
+											
+										<td width="30"></td>
+										<td width="1" bgcolor="#dcdcdc"></td>							
+									</tr>
+								</table><!-- End 2 Columns -->
+								
+								<!-- Shadow --> 
+								<table width="602" border="0" cellpadding="0" cellspacing="0" align="center">
+									<tr>
+										<td width="1" bgcolor="#dcdcdc"></td>
+										<td width="600" bgcolor="#dcdcdc">
+										
+										<!-- Middle Shadow -->
+										<table width="600" border="0" cellpadding="0" cellspacing="0" align="center">
+											<tr>
+												<td width="600" style="line-height: 1px;" valign="top">
+													<img src="images/middle_shadow.jpg" alt="" border="0" style="display: block;">								
+												</td>
+											</tr>
+										</table>
+										
+										</td>
+										<td width="1" bgcolor="#dcdcdc"></td>
+									</tr>
+								</table>
+								
+								<!-- Start 3 Column Images -->
+								<table width="602" border="0" cellpadding="0" cellspacing="0" align="center" bgcolor="#FFFFFF">
+									<tr>
+										<td width="1" bgcolor="#dcdcdc"></td>
+										<td width="31"></td>
+										<td width="538">
+											
+											
+											<!-- Empty Table -->
+											<table width="538" border="0" cellpadding="0" cellspacing="0" align="center" bgcolor="#FFFFFF">
+												<tr>
+													<td width="538" height="20" style="line-height: 1px;">
+														<img src="images/blank.gif" alt="" style="display: block;">
+													</td>
+												</tr>
+											</table>
+											
+											
+											<!-- Empty Table -->
+											<table width="538" border="0" cellpadding="0" cellspacing="0" align="center" bgcolor="#FFFFFF">
+												<tr>
+													<td width="538" height="8" style="line-height: 1px;">
+														<img src="images/blank.gif" alt="" style="display: block;">									
+													</td>
+												</tr>
+											</table>
+											
+											<!-- 3 Colums Text -->
+											<table width="538" border="0" cellpadding="0" cellspacing="0" align="center" bgcolor="#FFFFFF" style="font-size: 14px; color: #b8b8b8; text-align: left; font-family: Helvetica, Arial, sans-serif; line-height: 20px;">
+												<tr>
+													<td width="156" height="40" mc:edit="column_text1" valign="top"><a href="#" style="text-decoration: none; color: #707070; font-weight: bold;">'.$word[2]['word'].'</a> <br />'.$word[2]['translation'].'</td>
+													<td width="35" height="40"></td>
+													<td width="156" height="40" mc:edit="column_text2" valign="top"><a href="#" style="text-decoration: none; color: #707070; font-weight: bold;">'.$word[3]['word'].'</a> <br />'.$word[3]['translation'].'</td>
+													<td width="35" height="40"></td>
+													<td width="156" height="40" mc:edit="column_text3" valign="top"><a href="#" style="text-decoration: none; color: #707070; font-weight: bold;">'.$word[4]['word'].'</a> <br />'.$word[4]['translation'].'</td>
+												</tr>
+											</table><!-- End 3 Colums Text -->
+											
+											<!-- Preview Buttons -->
+											<table width="538" border="0" cellpadding="0" cellspacing="0" align="center" bgcolor="#FFFFFF">
+												<tr>
+													<td width="156" height="70 " bgcolor="#ffffff">
+														<a href="#"><img mc:edit="button" src="images/preview_button.jpg" alt="" border="0" style="display: block;"></a>
+													</td>
+													<td width="35" height="70"></td>
+													<td width="156" height="70" bgcolor="#ffffff">
+														<a href="#"><img mc:edit="button" src="images/preview_button.jpg" alt="" border="0" style="display: block;"></a>
+													</td>
+													<td width="35" height="70"></td>
+													<td width="156" height="70" bgcolor="#ffffff">
+														<a href="#"><img mc:edit="button" src="images/preview_button.jpg" alt="" border="0" style="display: block;"></a>
+													</td>
+												</tr>
+											</table><!-- End Preview Buttons -->
+											
+											<!-- Empty Table -->
+											<table width="538" border="0" cellpadding="0" cellspacing="0" align="center" bgcolor="#FFFFFF">
+												<tr>
+													<td width="538" height="20" style="line-height: 1px;">
+														<img src="images/blank.gif" alt="" style="display: block;">									
+													</td>
+												</tr>
+											</table>
+											
+											
+										</td>
+										<td width="31"></td>
+										<td width="1" bgcolor="#dcdcdc"></td>							
+									</tr>
+								</table><!-- End Start 3 Column Images -->';
+		return $header.$text.$footer;
+	}
   
 //This function gets the words from a lesson.
 //Input params: $language (Word language), $lesson (Name of the lesson)
